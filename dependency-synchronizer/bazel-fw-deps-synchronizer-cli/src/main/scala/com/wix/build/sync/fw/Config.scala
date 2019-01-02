a46962de595478760e5de27b4265010afd8d7b00
Natan Silnitsky <natans@wix.com>
2019-01-02 10:07:08 +0200

    fix maven of fw-deps-cli
    GitOrigin-RevId: bb336a44c271183e7be9f23def314d356a44f45c

---
+++ dependency-synchronizer/bazel-fw-deps-synchronizer-cli/src/main/scala/com/wix/build/sync/fw/Config.scala
@@ -0,0 +1,49 @@
+package com.wix.build.sync.fw
+
+case class Config(mavenRemoteRepositoryURL: List[String],
+                  managedDepsRepoUrl: String,
+                  fwArtifact: String,
+                  additionalDeps: List[String])
+
+object Config {
+  private val RepoFlag = "binary-repo"
+  //TODO: move to wix-maven-resolver module
+  private val wixRepos = List(
+    "http://repo.example.com:80/artifactory/libs-releases",
+    "http://repo.example.com:80/artifactory/libs-snapshots")
+
+  private val managedDepsRepoFlag = "managed_deps_repo"
+  private val fwLeafArtifactFlag = "fw-leaf-artifact"
+  private val additionalDepsFlag = "additional-deps"
+
+  private val Usage =
+    s"""Usage: DependencySynchronizer [--$RepoFlag remoteRepoUrl] --$managedDepsRepoFlag managedDepsRepoLocalPath --$fwLeafArtifactFlag fw-leaf-coordinates --additional-deps dep
+      |for example: --managed_deps_repo /path/to/managed/deps/repo com.wix.common:wix-framework-leaf:pom:1.0.0-SNAPSHOT --additional-dep com.wix:petri-aspects:1.0.0-SNAPSHOT
+    """.stripMargin
+
+  private val parser = new scopt.OptionParser[Config]("FWDependenciesSynchronizerCli") {
+    opt[String](RepoFlag)
+      .withFallback(() => wixRepos.mkString(","))
+      .action { case (remoteRepoUrls, config) =>
+        config.copy(mavenRemoteRepositoryURL = remoteRepoUrls.split(",").toList) }
+
+    opt[String](managedDepsRepoFlag)
+      .action { case (url, config) =>
+        config.copy(managedDepsRepoUrl = url) }
+
+    opt[String](fwLeafArtifactFlag)
+      .required()
+      .action { case (artifact, config) =>
+        config.copy(fwArtifact = artifact) }
+
+    opt[String](additionalDepsFlag)
+      .action { case (urls, config) =>
+        config.copy(additionalDeps = urls.split(",").toList) }
+  }
+
+  private val Empty = Config(null, null, null, List())
+
+  def parse(args: Array[String]): Config = {
+    parser.parse(args, Empty).getOrElse(throw new IllegalArgumentException(Usage))
+  }
+}
