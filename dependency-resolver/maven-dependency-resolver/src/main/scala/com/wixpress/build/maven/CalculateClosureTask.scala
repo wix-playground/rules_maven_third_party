07c6ae7a1482634c148ccb140073824d741c5af2
Natan Silnitsky <natans@wix.com>
2019-05-10 00:52:08 +0300

    move maven-dependency-resolver-server related code from public modules to private modules
    GitOrigin-RevId: dd916cb91929559062a90f8536042e131a88b9e6

--- dependency-resolver/maven-dependency-resolver/src/main/scala/com/wixpress/build/maven/CalculateClosureTask.scala
+++
@@ -1,6 +0,0 @@
-package com.wix.build.maven
-
-import com.wix.build.maven.dependency.resolver.api.v1.DependenciesClosureRequest
-import com.wix.hoopoe.ids.Guid
-
-case class CalculateClosureTask(jobId: Guid[CalculateClosureTask], request: DependenciesClosureRequest)
