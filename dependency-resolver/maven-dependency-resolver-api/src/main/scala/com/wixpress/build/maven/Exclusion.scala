ab778047b38bce95c66b9f411132570720126957
Or Shachar <ors@wix.com>
2020-05-04 18:32:04 +0300

    [deps reader] Now supports empty artifactId in serialized exclusions
    GitOrigin-RevId: 3bc1d8acdc0ccc3178c97fef425d5398fe9feeb3

--- dependency-resolver/maven-dependency-resolver-api/src/main/scala/com/wixpress/build/maven/Exclusion.scala
+++ dependency-resolver/maven-dependency-resolver-api/src/main/scala/com/wixpress/build/maven/Exclusion.scala
@@ -12,6 +12,7 @@ object Exclusion {
   def apply(dependency: Dependency): Exclusion = Exclusion(dependency.coordinates)
 
   def apply(serialized: String): Exclusion = serialized.split(":") match {
+    case Array(groupId) => Exclusion(groupId, "*")
     case Array(groupId, artifactId) => Exclusion(groupId, artifactId)
     case _ => throw new RuntimeException(s"Unsupported exclusion format '$serialized'")
   }
