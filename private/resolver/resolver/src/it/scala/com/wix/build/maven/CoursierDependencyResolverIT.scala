f27a9130de25f22d2f7ce5b21aa77b8211a66fc8
Vaidas Pilkauskas <vaidasp@wix.com>
2022-02-18 14:52:16 +0200

    Move resolver and resolver testkit to private for OSS
    GitOrigin-RevId: ee39d802a8aa04f6d0a44d4146f1c8552ec64477

---
+++ private/resolver/resolver/src/it/scala/com/wix/build/maven/CoursierDependencyResolverIT.scala
@@ -0,0 +1,58 @@
+package com.wix.build.maven
+
+import com.wix.build.maven._
+import com.wix.build.maven.ArtifactDescriptor.anArtifact
+import com.wix.build.maven.MavenMakers.{aDependency, randomDependency}
+import org.specs2.mutable.SpecificationWithJUnit
+import org.specs2.specification.{AfterEach, Scope}
+
+//noinspection TypeAnnotation
+class CoursierDependencyResolverIT extends SpecificationWithJUnit with AfterEach {
+  sequential
+  val fakeMavenRepository = new FakeMavenRepository()
+
+  "return only one entry for each dependency given transitive dependency has different scope" in new Context {
+    override def transitiveRoot = aDependency("transitive").withScope(MavenScope.Runtime)
+
+    val nodes = mavenDependencyResolver.dependencyClosureOf(
+      List(dependency, transitiveRoot.withScope(MavenScope.Compile)),
+      emptyManagedDependencies
+    )
+
+    nodes.filter(_.baseDependency == dependency) must have size 1
+    nodes.filter(_.baseDependency.coordinates == transitiveRoot.coordinates) must have size 1
+  }
+
+  "given dependency that is not in remote repository must not explode" in new Context {
+    val notExistsDependency = randomDependency()
+
+    override def remoteArtifacts: Set[ArtifactDescriptor] = Set.empty
+
+    mavenDependencyResolver.dependencyClosureOf(List(notExistsDependency), emptyManagedDependencies) must beEmpty
+  }
+
+  trait Context extends Scope {
+    def transitiveRoot = aDependency("transitive")
+
+    def dependency = aDependency("dep")
+
+    def remoteArtifacts: Set[ArtifactDescriptor] = Set(
+      anArtifact(dependency.coordinates).withDependency(transitiveRoot),
+      anArtifact(transitiveRoot.coordinates)
+    )
+
+    def emptyManagedDependencies = List.empty[Dependency]
+
+    def mavenDependencyResolver = resolverBasedOn(remoteArtifacts)
+  }
+
+  def resolverBasedOn(artifacts: Set[ArtifactDescriptor]) = {
+    fakeMavenRepository.addArtifacts(artifacts)
+    fakeMavenRepository.start()
+    new CoursierDependencyResolver(List(fakeMavenRepository.url))
+  }
+
+  override protected def after = {
+    fakeMavenRepository.stop()
+  }
+}
---
+++ private/resolver/resolver/src/main/scala/com/wix/build/maven/CoursierDependencyResolver.scala
@@ -0,0 +1,88 @@
+package com.wix.build.maven
+
+import CoursierDependencyResolver._
+
+import coursier.MavenRepository
+import coursier.cache.Cache
+import coursier.core.{Attributes, Classifier, Configuration, Module, ModuleName, Organization, Resolution, ResolutionProcess, Type, Dependency => CoursierDependency}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+
+class CoursierDependencyResolver(remoteRepoURLs: => List[String]) extends MavenDependencyResolver {
+  override def managedDependenciesOf(artifact: Coordinates): List[Dependency] = ???
+
+  override def directDependenciesOf(artifact: Coordinates): List[Dependency] = ???
+
+  override def dependencyClosureOf(baseDependencies: List[Dependency],
+                                   withManagedDependencies: List[Dependency],
+                                   ignoreMissingDependencies: Boolean): Set[DependencyNode] = {
+
+    val repositories = remoteRepoURLs.map(repo => MavenRepository(repo))
+    val dependencies = (baseDependencies ++ withManagedDependencies).map(toCoursierDependency)
+
+    val fetch = ResolutionProcess.fetch(repositories, Cache.default.fetch)
+
+    val resolution = Resolution()
+      .withRootDependencies(dependencies)
+
+    // not found packages are under result.errors
+    val result = resolution.process.run(fetch).unsafeRun()
+
+    result.finalDependenciesCache.map(toDependencyNode).toSet
+  }
+
+}
+
+object CoursierDependencyResolver {
+
+  def toDependencyNode(result: (CoursierDependency, Seq[CoursierDependency])): DependencyNode = {
+    DependencyNode(toDependency(result._1), result._2.map(toDependency).toSet)
+  }
+
+  def toCoursierModule(dependency: Dependency): Module = {
+    Module(Organization(dependency.coordinates.groupId), ModuleName(dependency.coordinates.artifactId), Map.empty)
+  }
+
+  def toCoursierDependency(dependency: Dependency): CoursierDependency =
+    CoursierDependency(toCoursierModule(dependency), dependency.version)
+      .withConfiguration(Configuration(dependency.scope.name))
+      .withExclusions(dependency.exclusions.map(toCoursierExclusion))
+      .withAttributes(
+        Attributes(
+          Type(dependency.coordinates.packaging.value),
+          dependency.coordinates.classifier.map(Classifier(_)).getOrElse(Classifier.empty)
+        )
+      )
+
+  def toCoordinates(dependency: CoursierDependency): Coordinates = {
+    import dependency._
+
+    val packaging = if (attributes.packaging.value.nonEmpty)
+      Packaging(attributes.packaging.value)
+    else
+      Packaging("jar")
+
+    val classifier = if (attributes.classifier.value.nonEmpty)
+      Some(attributes.classifier.value)
+    else
+      None
+
+    Coordinates(module.organization.value, module.name.value, version, packaging, classifier)
+  }
+
+  def toExclusion(coursierExclusion: (Organization, ModuleName)): Exclusion = {
+    Exclusion(coursierExclusion._1.value, coursierExclusion._2.value)
+  }
+
+  def toCoursierExclusion(exclusion: Exclusion): (Organization, ModuleName) =
+    Organization(exclusion.groupId) -> ModuleName(exclusion.artifactId)
+
+  def toDependency(dependency: CoursierDependency): Dependency =
+    Dependency(
+      toCoordinates(dependency),
+      MavenScope.of(dependency.configuration.value),
+      isNeverLink = false,
+      exclusions = dependency.exclusions.map(toExclusion)
+    )
+}
