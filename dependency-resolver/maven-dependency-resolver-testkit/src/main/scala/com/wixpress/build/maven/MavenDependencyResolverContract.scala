wix-incubator/exodus

fix bug to not tolerate non-existing maven coords (unless ignoreMissingDependencies flag is explciitly passed)
GitOrigin-RevId: ff21adeeca3794a8b47bd8754790c57dd1a67f11

dependency-resolver/maven-dependency-resolver-testkit/src/main/scala/com/wixpress/build/maven/MavenDependencyResolverContract.scala
@@ -1,6 +1,6 @@
 package com.wixpress.build.maven
 
-import com.wixpress.build.maven.ArtifactDescriptor.anArtifact
+import com.wixpress.build.maven.ArtifactDescriptor.{anArtifact, rootFor}
 import com.wixpress.build.maven.MavenMakers._
 import org.specs2.mutable.SpecificationWithJUnit
 import org.specs2.specification.Scope
@@ -146,7 +146,8 @@ abstract class MavenDependencyResolverContract extends SpecificationWithJUnit {
 
         def directDependency = Dependency(someCoordinates("direct"), MavenScope.Compile)
 
-        override def remoteArtifacts = Set(anArtifact(interestingArtifact).withDependency(directDependency))
+        override def remoteArtifacts = Set(anArtifact(interestingArtifact).withDependency(directDependency),
+          rootFor(directDependency.coordinates))
 
         mavenDependencyResolver.allDependenciesOf(interestingArtifact) must contain(directDependency)
       }
@@ -412,15 +413,6 @@ abstract class MavenDependencyResolverContract extends SpecificationWithJUnit {
             DependencyNode(dependencies.head, Set.empty)
           )
         }
-      }
-
-      "given dependency that is not in remote repository must return root dependencyNode" in new ctx {
-        val notExistsDependency = randomDependency()
-
-        override def remoteArtifacts: Set[ArtifactDescriptor] = Set.empty
-
-        mavenDependencyResolver.dependencyClosureOf(Set(notExistsDependency), emptyManagedDependencies) must contain(
-          DependencyNode(notExistsDependency, Set.empty))
       }
 
     }

dependency-resolver/maven-dependency-resolver/src/it/scala/com/wixpress/build/maven/AetherMavenDependencyResolverIT.scala
@@ -1,7 +1,7 @@
 package com.wixpress.build.maven
 
 import com.wixpress.build.maven.ArtifactDescriptor.anArtifact
-import com.wixpress.build.maven.MavenMakers.aDependency
+import com.wixpress.build.maven.MavenMakers.{aDependency, randomDependency}
 import org.specs2.specification.AfterEach
 
 //noinspection TypeAnnotation
@@ -23,6 +23,14 @@ class AetherMavenDependencyResolverIT extends MavenDependencyResolverContract wi
     val nodes = mavenDependencyResolver.dependencyClosureOf(Set(dependency, transitiveRoot.withScope(MavenScope.Compile)), emptyManagedDependencies)
     nodes.filter(_.baseDependency == dependency) must have size 1
     nodes.filter(_.baseDependency.coordinates == transitiveRoot.coordinates) must have size 1
+  }
+
+  "given dependency that is not in remote repository must explode" in new ctx {
+    val notExistsDependency = randomDependency()
+
+    override def remoteArtifacts: Set[ArtifactDescriptor] = Set.empty
+
+    mavenDependencyResolver.dependencyClosureOf(Set(notExistsDependency), emptyManagedDependencies) must throwA[IllegalArgumentException]
   }
 
   trait singleDependencyWithSingleDependency extends ctx {

wix-playground/rules_maven_third_party

dependency-resolver/maven-dependency-resolver-testkit/src/main/scala/com/wixpress/build/maven/MavenDependencyResolverContract.scala
@@ -1,6 +1,6 @@
 package com.wix.build.maven
 
-import com.wix.build.maven.ArtifactDescriptor.anArtifact
+import com.wix.build.maven.ArtifactDescriptor.{anArtifact, rootFor}
 import com.wix.build.maven.MavenMakers._
 import org.specs2.mutable.SpecificationWithJUnit
 import org.specs2.specification.Scope
@@ -146,7 +146,8 @@ abstract class MavenDependencyResolverContract extends SpecificationWithJUnit {
 
         def directDependency = Dependency(someCoordinates("direct"), MavenScope.Compile)
 
-        override def remoteArtifacts = Set(anArtifact(interestingArtifact).withDependency(directDependency))
+        override def remoteArtifacts = Set(anArtifact(interestingArtifact).withDependency(directDependency),
+          rootFor(directDependency.coordinates))
 
         mavenDependencyResolver.allDependenciesOf(interestingArtifact) must contain(directDependency)
       }
@@ -412,15 +413,6 @@ abstract class MavenDependencyResolverContract extends SpecificationWithJUnit {
             DependencyNode(dependencies.head, Set.empty)
           )
         }
-      }
-
-      "given dependency that is not in remote repository must return root dependencyNode" in new ctx {
-        val notExistsDependency = randomDependency()
-
-        override def remoteArtifacts: Set[ArtifactDescriptor] = Set.empty
-
-        mavenDependencyResolver.dependencyClosureOf(Set(notExistsDependency), emptyManagedDependencies) must contain(
-          DependencyNode(notExistsDependency, Set.empty))
       }
 
     }

dependency-resolver/maven-dependency-resolver/src/it/scala/com/wixpress/build/maven/AetherMavenDependencyResolverIT.scala
@@ -1,7 +1,7 @@
 package com.wix.build.maven
 
 import com.wix.build.maven.ArtifactDescriptor.anArtifact
-import com.wix.build.maven.MavenMakers.aDependency
+import com.wix.build.maven.MavenMakers.{aDependency, randomDependency}
 import org.specs2.specification.AfterEach
 
 //noinspection TypeAnnotation
@@ -23,6 +23,14 @@ class AetherMavenDependencyResolverIT extends MavenDependencyResolverContract wi
     val nodes = mavenDependencyResolver.dependencyClosureOf(Set(dependency, transitiveRoot.withScope(MavenScope.Compile)), emptyManagedDependencies)
     nodes.filter(_.baseDependency == dependency) must have size 1
     nodes.filter(_.baseDependency.coordinates == transitiveRoot.coordinates) must have size 1
+  }
+
+  "given dependency that is not in remote repository must explode" in new ctx {
+    val notExistsDependency = randomDependency()
+
+    override def remoteArtifacts: Set[ArtifactDescriptor] = Set.empty
+
+    mavenDependencyResolver.dependencyClosureOf(Set(notExistsDependency), emptyManagedDependencies) must throwA[IllegalArgumentException]
   }
 
   trait singleDependencyWithSingleDependency extends ctx {