wix-incubator/exodus

Support master-parent dependency management
dependencies to sync should be given explicitly and not from artifact
- synchronizer now syncs between given set of dependencies and target workspace
- the first argument is used to fetch managed dependencies to set transitive undeclared dependencies version
- introduced a new utility for collecting dependencies from different sources (explicit lists / managed artifact)

* managed version overrides declared versions when evaluating transitive dependencies

* Migrator now utilize master parent as dep management for synchronizer

GitOrigin-RevId: 749044f675eaeaad98179e188f94968aca19f557

dependency-resolver/maven-dependency-resolver/src/main/scala/com/wixpress/build/maven/DependencyCollector.scala
@@ -1,0 +1,21 @@
+package com.wixpress.build.maven
+
+class DependencyCollector(resolver: MavenDependencyResolver, dependencies: Set[Dependency] = Set.empty) {
+
+  def withManagedDependenciesOf(artifact: Coordinates): DependencyCollector =
+    addOrOverrideDependencies(resolver.managedDependenciesOf(artifact))
+
+  def addOrOverrideDependencies(newDependencies: Set[Dependency]) =
+    new DependencyCollector(resolver, dependencies.addOrOverride(newDependencies))
+
+  def dependencySet(): Set[Dependency] = dependencies
+
+  implicit class `add or override dependency set`(originalSet: Set[Dependency]) {
+    def addOrOverride(otherSet: Set[Dependency]): Set[Dependency] = {
+      val setWithoutOverridenDependencies = originalSet.filterNot(originalDependency =>
+        otherSet.map(_.coordinates).exists(_.equalsIgnoringVersion(originalDependency.coordinates)))
+      setWithoutOverridenDependencies ++ otherSet
+    }
+  }
+
+}

dependency-resolver/maven-dependency-resolver/src/test/scala/com/wixpress/build/maven/DependencyCollectorTest.scala
@@ -1,0 +1,72 @@
+package com.wixpress.build.maven
+
+import com.wixpress.build.maven.MavenMakers.aDependency
+import org.specs2.mutable.SpecificationWithJUnit
+import org.specs2.specification.Scope
+
+//noinspection TypeAnnotation
+class DependencyCollectorTest extends SpecificationWithJUnit {
+
+  trait ctx extends Scope {
+    def emptyResolver = new FakeMavenDependencyResolver(Set.empty, Set.empty)
+
+    def resolverWithManagedDependencies(managedDependencies: Set[Dependency]) =
+      new FakeMavenDependencyResolver(managedDependencies, Set.empty)
+
+    def artifactWithManagedDependencies = MavenMakers.someCoordinates("managed")
+  }
+
+  "DependencyCollector" >> {
+    "when no new dependencies were added after initialization" should {
+      "return empty dependency set" in new ctx {
+        val collector = new DependencyCollector(emptyResolver)
+        collector.dependencySet() mustEqual Set.empty[Dependency]
+      }
+
+      "return a set with dependencies after they were added using the addOrOverrideDependencies call" in new ctx {
+        val collector = new DependencyCollector(emptyResolver)
+        val newDependencies = Set(aDependency("a"))
+
+        collector
+          .addOrOverrideDependencies(newDependencies)
+          .dependencySet() must contain(allOf(newDependencies))
+      }
+
+      "return a set with dependencies that were extracted from managed dependencies of given artifact" in new ctx {
+        val newDependencies = Set(aDependency("b"))
+        val collector = new DependencyCollector(resolverWithManagedDependencies(newDependencies))
+
+        collector
+          .withManagedDependenciesOf(artifactWithManagedDependencies)
+          .dependencySet() must contain(allOf(newDependencies))
+      }
+    }
+
+    "after already collect dependency A," should {
+      trait oneCollectedDependencyCtx extends ctx {
+        val existingDependency: Dependency = aDependency("existing")
+        def resolver: MavenDependencyResolver = emptyResolver
+        def collector = new DependencyCollector(resolver, Set(existingDependency))
+      }
+
+      "return a set with both A and new dependencies after they were added using the with dependencies call" in new oneCollectedDependencyCtx {
+        val newDependency = aDependency("new")
+        collector
+          .addOrOverrideDependencies(Set(newDependency))
+          .dependencySet() mustEqual Set(existingDependency, newDependency)
+      }
+
+      "allow overriding the version of A by adding a set with a different version of A" in new oneCollectedDependencyCtx {
+        val newDependency = existingDependency.withVersion("different-version")
+
+        collector
+          .addOrOverrideDependencies(Set(newDependency))
+          .dependencySet() mustEqual Set(newDependency)
+      }
+
+      // TODO: same tests for dependencies added from managed dependencies coordinates
+
+    }
+
+  }
+}

wix-playground/rules_maven_third_party

dependency-resolver/maven-dependency-resolver/src/main/scala/com/wixpress/build/maven/DependencyCollector.scala
@@ -1,0 +1,21 @@
+package com.wix.build.maven
+
+class DependencyCollector(resolver: MavenDependencyResolver, dependencies: Set[Dependency] = Set.empty) {
+
+  def withManagedDependenciesOf(artifact: Coordinates): DependencyCollector =
+    addOrOverrideDependencies(resolver.managedDependenciesOf(artifact))
+
+  def addOrOverrideDependencies(newDependencies: Set[Dependency]) =
+    new DependencyCollector(resolver, dependencies.addOrOverride(newDependencies))
+
+  def dependencySet(): Set[Dependency] = dependencies
+
+  implicit class `add or override dependency set`(originalSet: Set[Dependency]) {
+    def addOrOverride(otherSet: Set[Dependency]): Set[Dependency] = {
+      val setWithoutOverridenDependencies = originalSet.filterNot(originalDependency =>
+        otherSet.map(_.coordinates).exists(_.equalsIgnoringVersion(originalDependency.coordinates)))
+      setWithoutOverridenDependencies ++ otherSet
+    }
+  }
+
+}

dependency-resolver/maven-dependency-resolver/src/test/scala/com/wixpress/build/maven/DependencyCollectorTest.scala
@@ -1,0 +1,72 @@
+package com.wix.build.maven
+
+import com.wix.build.maven.MavenMakers.aDependency
+import org.specs2.mutable.SpecificationWithJUnit
+import org.specs2.specification.Scope
+
+//noinspection TypeAnnotation
+class DependencyCollectorTest extends SpecificationWithJUnit {
+
+  trait ctx extends Scope {
+    def emptyResolver = new FakeMavenDependencyResolver(Set.empty, Set.empty)
+
+    def resolverWithManagedDependencies(managedDependencies: Set[Dependency]) =
+      new FakeMavenDependencyResolver(managedDependencies, Set.empty)
+
+    def artifactWithManagedDependencies = MavenMakers.someCoordinates("managed")
+  }
+
+  "DependencyCollector" >> {
+    "when no new dependencies were added after initialization" should {
+      "return empty dependency set" in new ctx {
+        val collector = new DependencyCollector(emptyResolver)
+        collector.dependencySet() mustEqual Set.empty[Dependency]
+      }
+
+      "return a set with dependencies after they were added using the addOrOverrideDependencies call" in new ctx {
+        val collector = new DependencyCollector(emptyResolver)
+        val newDependencies = Set(aDependency("a"))
+
+        collector
+          .addOrOverrideDependencies(newDependencies)
+          .dependencySet() must contain(allOf(newDependencies))
+      }
+
+      "return a set with dependencies that were extracted from managed dependencies of given artifact" in new ctx {
+        val newDependencies = Set(aDependency("b"))
+        val collector = new DependencyCollector(resolverWithManagedDependencies(newDependencies))
+
+        collector
+          .withManagedDependenciesOf(artifactWithManagedDependencies)
+          .dependencySet() must contain(allOf(newDependencies))
+      }
+    }
+
+    "after already collect dependency A," should {
+      trait oneCollectedDependencyCtx extends ctx {
+        val existingDependency: Dependency = aDependency("existing")
+        def resolver: MavenDependencyResolver = emptyResolver
+        def collector = new DependencyCollector(resolver, Set(existingDependency))
+      }
+
+      "return a set with both A and new dependencies after they were added using the with dependencies call" in new oneCollectedDependencyCtx {
+        val newDependency = aDependency("new")
+        collector
+          .addOrOverrideDependencies(Set(newDependency))
+          .dependencySet() mustEqual Set(existingDependency, newDependency)
+      }
+
+      "allow overriding the version of A by adding a set with a different version of A" in new oneCollectedDependencyCtx {
+        val newDependency = existingDependency.withVersion("different-version")
+
+        collector
+          .addOrOverrideDependencies(Set(newDependency))
+          .dependencySet() mustEqual Set(newDependency)
+      }
+
+      // TODO: same tests for dependencies added from managed dependencies coordinates
+
+    }
+
+  }
+}