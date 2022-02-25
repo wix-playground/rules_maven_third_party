wix-incubator/exodus

renamed first level directories to not include bazel- prefix
GitOrigin-RevId: 82000687a0cdddf6870b0a81edf26e18e6e26300

dependency-resolver/maven-dependency-resolver-api/src/main/scala/com/wixpress/build/maven/Coordinates.scala
@@ -1,0 +1,102 @@
+package com.wixpress.build.maven
+
+import org.eclipse.aether.artifact.{Artifact, DefaultArtifact}
+
+case class Coordinates(groupId: String,
+                       artifactId: String,
+                       version: String,
+                       packaging: Option[String] = Some("jar"),
+                       classifier: Option[String] = None
+                      ) {
+
+  def equalsOnGroupIdAndArtifactId(otherCoordinates: Coordinates): Boolean = {
+    this.groupId == otherCoordinates.groupId &&
+      this.artifactId == otherCoordinates.artifactId
+  }
+
+  private def serializeOptional(optional: Option[String]) = optional.map(string => s":$string").getOrElse("")
+
+  def serialized: String = s"$groupId:$artifactId" +
+    s"${serializeOptional(packaging.filterNot(classifier.isEmpty && _ == "jar"))}" +
+    s"${serializeOptional(classifier)}:$version"
+
+  def asTopic: String = serialized.replace(":", "_")
+
+  def asAetherArtifact: DefaultArtifact = new DefaultArtifact(serialized)
+
+  def workspaceRuleName: String = {
+    val groupIdPart = fixNameToBazelConventions(groupId)
+    val artifactIdPart = fixNameToBazelConventions(artifactId)
+    val classifierPart = classifier.map(c => s"_${fixNameToBazelConventions(c)}").getOrElse("")
+    s"${groupIdPart}_$artifactIdPart" + classifierPart
+  }
+
+  def libraryRuleName: String = {
+    val artifactIdPart = fixNameToBazelConventions(artifactId)
+    val classifierPart = classifier.map(c => s"_${fixNameToBazelConventions(c)}").getOrElse("")
+    s"$artifactIdPart$classifierPart"
+  }
+
+  private def fixNameToBazelConventions(id: String): String = {
+    id.replace('-', '_').replace('.', '_')
+  }
+
+  def equalsIgnoringVersion(otherCoordinates: Coordinates): Boolean =
+    this.groupId == otherCoordinates.groupId &&
+      this.artifactId == otherCoordinates.artifactId &&
+      this.packaging == otherCoordinates.packaging &&
+      this.classifier == otherCoordinates.classifier
+}
+
+object Coordinates {
+
+  def fromAetherArtifact(artifact: Artifact): Coordinates =
+    Coordinates(
+      artifact.getGroupId,
+      artifact.getArtifactId,
+      artifact.getVersion,
+      Option(artifact.getExtension).filter(!_.isEmpty),
+      Option(artifact.getClassifier).filter(!_.isEmpty))
+
+  def deserialize(serialized: String): Coordinates = {
+    val coordinates = serialized.split(':')
+
+    val groupId = 0
+    val artifactId = 1
+    val packaging = 2
+
+    coordinates.length match {
+      case 3 =>
+        val version = 2
+        Coordinates(
+          groupId = coordinates(groupId),
+          artifactId = coordinates(artifactId),
+          version = coordinates(version)
+        )
+
+      case 4 =>
+        val version = 3
+        Coordinates(
+          groupId = coordinates(groupId),
+          artifactId = coordinates(artifactId),
+          version = coordinates(version),
+          packaging = Some(coordinates(packaging))
+        )
+
+      case 5 =>
+        val classifier = 3
+        val version = 4
+        Coordinates(
+          groupId = coordinates(groupId),
+          artifactId = coordinates(artifactId),
+          version = coordinates(version),
+          packaging = Some(coordinates(packaging)),
+          classifier = Some(coordinates(classifier))
+        )
+    }
+
+
+  }
+
+
+}

dependency-resolver/maven-dependency-resolver-api/src/main/scala/com/wixpress/build/maven/MavenScope.scala
@@ -1,0 +1,19 @@
+package com.wixpress.build.maven
+
+abstract class MavenScope(val name: String)
+
+object MavenScope {
+
+  case object Runtime extends MavenScope("runtime")
+
+  case object Compile extends MavenScope("compile")
+
+  case object Test extends MavenScope("test")
+
+  case object Provided extends MavenScope("provided")
+
+  private val allScopes = List(Runtime, Compile, Test, Provided)
+
+  def of(scopeName: String): MavenScope = allScopes.find(_.name.equalsIgnoreCase(scopeName))
+    .getOrElse(MavenScope.Compile)
+}

dependency-resolver/maven-dependency-resolver-api/src/test/scala/com/wixpress/build/maven/DependencyTest.scala
@@ -1,0 +1,27 @@
+package com.wixpress.build.maven
+
+import org.specs2.mutable.SpecificationWithJUnit
+import org.eclipse.aether.graph.{Dependency => AetherDependency}
+
+class DependencyTest extends SpecificationWithJUnit {
+  val coordinates = new Coordinates("some.group","some-artifact","some-version")
+  "Dependency" should {
+    "convert from aether dependency" in {
+      val aetherDep = new AetherDependency(coordinates.asAetherArtifact,"")
+      Dependency.fromAetherDependency(aetherDep) mustEqual Dependency(coordinates,MavenScope.Compile)
+    }
+
+    "convert to aether dependency" in {
+      val dependency = Dependency(coordinates,MavenScope.Compile)
+
+      val aetherDependency = dependency.asAetherDependency
+
+      aetherDependency.getArtifact.getGroupId mustEqual coordinates.groupId
+      aetherDependency.getArtifact.getArtifactId mustEqual coordinates.artifactId
+      aetherDependency.getArtifact.getVersion mustEqual coordinates.version
+      aetherDependency.getArtifact.getClassifier mustEqual coordinates.classifier.getOrElse("")
+      aetherDependency.getArtifact.getExtension mustEqual coordinates.packaging.get
+      aetherDependency.getScope mustEqual dependency.scope.name
+    }
+  }
+}

dependency-resolver/maven-dependency-resolver-testkit/src/main/scala/com/wixpress/build/maven/FakeMavenRepository.scala
@@ -1,0 +1,54 @@
+package com.wixpress.build.maven
+
+import java.io.ByteArrayInputStream
+
+import org.apache.commons.codec.digest.DigestUtils
+import org.codehaus.mojo.mrm.api.maven.Artifact
+import org.codehaus.mojo.mrm.impl.maven.{ArtifactStoreFileSystem, MemoryArtifactStore}
+import org.codehaus.mojo.mrm.plugin.FileSystemServer
+
+class FakeMavenRepository(port: Int = 0) {
+
+  implicit class ExtendedArtifactDescriptor(artifact: ArtifactDescriptor) {
+    def asArtifact(ofType: String): Artifact = {
+      val parent = artifact.parentCoordinates
+      val groupId = artifact.groupId
+        .getOrElse(parent.map(_.groupId).getOrElse( throw new RuntimeException("missing groupId or parent.groupId")))
+      val version =  artifact.version
+        .getOrElse(parent.map(_.version).getOrElse( throw new RuntimeException("missing version or parent.version")))
+      new Artifact(groupId, artifact.artifactId, version, ofType)
+    }
+  }
+
+  private val inMemoryArtifactStore = new MemoryArtifactStore
+  private val mavenRepoManager = new FileSystemServer("foo", port,
+    new ArtifactStoreFileSystem(inMemoryArtifactStore), "")
+
+  def url: String = mavenRepoManager.getUrl
+
+  def start(): Unit = mavenRepoManager.ensureStarted()
+
+  def stop(): Unit = {
+    mavenRepoManager.finish()
+    mavenRepoManager.waitForFinished()
+  }
+
+  def addArtifacts(artifact: ArtifactDescriptor*): Unit = addArtifacts(artifact.toSet)
+
+  def addArtifacts(artifacts: Set[ArtifactDescriptor]): Unit = artifacts.foreach(addSingleArtifact)
+
+  def addSingleArtifact(artifact: ArtifactDescriptor): Unit = {
+    val xml = artifact.pomXml
+    val md5 = DigestUtils.md5Hex(xml)
+    val sha1 = DigestUtils.sha1Hex(xml)
+    inMemoryArtifactStore.set(artifact.asArtifact(ofType = "pom"), streamFrom(xml))
+    inMemoryArtifactStore.set(artifact.asArtifact(ofType = "pom.md5"), streamFrom(md5))
+    inMemoryArtifactStore.set(artifact.asArtifact(ofType = "pom.sha1"), streamFrom(sha1))
+  }
+
+
+  private def streamFrom(input: String) = {
+    new ByteArrayInputStream(input.getBytes("UTF-8"))
+  }
+
+}

dependency-resolver/maven-dependency-resolver-testkit/src/test/scala/com/wixpress/build/maven/ArtifactDescriptorTest.scala
@@ -1,0 +1,60 @@
+package com.wixpress.build.maven
+
+import org.specs2.mutable.SpecificationWithJUnit
+import org.specs2.matcher.XmlMatchers._
+
+import scala.xml._
+
+class ArtifactDescriptorTest extends SpecificationWithJUnit {
+  val baseCoordiantes = Coordinates("some.group", "some-artifact", "some-version")
+  val baseDescriptor = ArtifactDescriptor.anArtifact(baseCoordiantes)
+  val depCoordinates = Coordinates("some.group", "some-dep", "other-version")
+  val dependency = Dependency(depCoordinates, MavenScope.Compile)
+
+  "ArtifactDescriptor" should {
+    "reconstruct itself with new  dependency" in {
+      baseDescriptor.withDependency(dependency).dependencies must contain(dependency)
+    }
+
+    "reconstruct itself with new managed dependency" in {
+      baseDescriptor.withManagedDependency(dependency).managedDependencies must contain(dependency)
+    }
+
+    "build pom.xml" in {
+      val expected: Elem = <project xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd"
+                                    xmlns="http://maven.apache.org/POM/4.0.0"
+                                    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+                            <modelVersion>4.0.0</modelVersion>
+                            <groupId>{baseCoordiantes.groupId}</groupId>
+                            <artifactId>{baseCoordiantes.artifactId}</artifactId>
+                            <version>{baseCoordiantes.version}</version>
+                          </project>
+
+      val pom: Elem = XML.loadString(baseDescriptor.pomXml)
+
+      pom must beEqualToIgnoringSpace(expected)
+    }
+
+    "build pom.xml with dependency and exclusions" in {
+      val exclusion = Exclusion("excluded.group","excluded-artifact")
+      val dependencyWithExclusions = dependency.copy(exclusions=Set(exclusion))
+      val expected = <dependency>
+        <groupId>{dependencyWithExclusions.coordinates.groupId}</groupId>
+        <artifactId>{dependencyWithExclusions.coordinates.artifactId}</artifactId>
+        <version>{dependencyWithExclusions.coordinates.version}</version>
+        <scope>{dependencyWithExclusions.scope.name}</scope>
+        <exclusions>
+          <exclusion>
+            <artifactId>{exclusion.artifactId}</artifactId>
+            <groupId>{exclusion.groupId}</groupId>
+          </exclusion>
+        </exclusions>
+      </dependency>
+      val pom: Elem = XML.loadString(baseDescriptor.withDependency(dependencyWithExclusions).pomXml)
+      val allDeps = pom \ "dependencies" \ "dependency"
+
+      allDeps aka "dependencies in generated pom" must not be empty
+      allDeps.head must beEqualToIgnoringSpace(expected)
+    }
+  }
+}

dependency-resolver/maven-dependency-resolver/src/it/scala/com/wixpress/build/maven/AetherMavenDependencyResolverIT.scala
@@ -1,0 +1,19 @@
+package com.wixpress.build.maven
+
+import org.specs2.specification.AfterEach
+
+//noinspection TypeAnnotation
+class AetherMavenDependencyResolverIT extends MavenDependencyResolverContract with AfterEach {
+  sequential
+  val fakeMavenRepository = new FakeMavenRepository()
+
+  override def resolverBasedOn(artifacts: Set[ArtifactDescriptor]) = {
+    fakeMavenRepository.addArtifacts(artifacts)
+    fakeMavenRepository.start()
+    new AetherMavenDependencyResolver(List(fakeMavenRepository.url))
+  }
+
+  override protected def after = {
+    fakeMavenRepository.stop()
+  }
+}

wix-playground/rules_maven_third_party

dependency-resolver/maven-dependency-resolver-api/src/main/scala/com/wixpress/build/maven/Coordinates.scala
@@ -1,0 +1,102 @@
+package com.wix.build.maven
+
+import org.eclipse.aether.artifact.{Artifact, DefaultArtifact}
+
+case class Coordinates(groupId: String,
+                       artifactId: String,
+                       version: String,
+                       packaging: Option[String] = Some("jar"),
+                       classifier: Option[String] = None
+                      ) {
+
+  def equalsOnGroupIdAndArtifactId(otherCoordinates: Coordinates): Boolean = {
+    this.groupId == otherCoordinates.groupId &&
+      this.artifactId == otherCoordinates.artifactId
+  }
+
+  private def serializeOptional(optional: Option[String]) = optional.map(string => s":$string").getOrElse("")
+
+  def serialized: String = s"$groupId:$artifactId" +
+    s"${serializeOptional(packaging.filterNot(classifier.isEmpty && _ == "jar"))}" +
+    s"${serializeOptional(classifier)}:$version"
+
+  def asTopic: String = serialized.replace(":", "_")
+
+  def asAetherArtifact: DefaultArtifact = new DefaultArtifact(serialized)
+
+  def workspaceRuleName: String = {
+    val groupIdPart = fixNameToBazelConventions(groupId)
+    val artifactIdPart = fixNameToBazelConventions(artifactId)
+    val classifierPart = classifier.map(c => s"_${fixNameToBazelConventions(c)}").getOrElse("")
+    s"${groupIdPart}_$artifactIdPart" + classifierPart
+  }
+
+  def libraryRuleName: String = {
+    val artifactIdPart = fixNameToBazelConventions(artifactId)
+    val classifierPart = classifier.map(c => s"_${fixNameToBazelConventions(c)}").getOrElse("")
+    s"$artifactIdPart$classifierPart"
+  }
+
+  private def fixNameToBazelConventions(id: String): String = {
+    id.replace('-', '_').replace('.', '_')
+  }
+
+  def equalsIgnoringVersion(otherCoordinates: Coordinates): Boolean =
+    this.groupId == otherCoordinates.groupId &&
+      this.artifactId == otherCoordinates.artifactId &&
+      this.packaging == otherCoordinates.packaging &&
+      this.classifier == otherCoordinates.classifier
+}
+
+object Coordinates {
+
+  def fromAetherArtifact(artifact: Artifact): Coordinates =
+    Coordinates(
+      artifact.getGroupId,
+      artifact.getArtifactId,
+      artifact.getVersion,
+      Option(artifact.getExtension).filter(!_.isEmpty),
+      Option(artifact.getClassifier).filter(!_.isEmpty))
+
+  def deserialize(serialized: String): Coordinates = {
+    val coordinates = serialized.split(':')
+
+    val groupId = 0
+    val artifactId = 1
+    val packaging = 2
+
+    coordinates.length match {
+      case 3 =>
+        val version = 2
+        Coordinates(
+          groupId = coordinates(groupId),
+          artifactId = coordinates(artifactId),
+          version = coordinates(version)
+        )
+
+      case 4 =>
+        val version = 3
+        Coordinates(
+          groupId = coordinates(groupId),
+          artifactId = coordinates(artifactId),
+          version = coordinates(version),
+          packaging = Some(coordinates(packaging))
+        )
+
+      case 5 =>
+        val classifier = 3
+        val version = 4
+        Coordinates(
+          groupId = coordinates(groupId),
+          artifactId = coordinates(artifactId),
+          version = coordinates(version),
+          packaging = Some(coordinates(packaging)),
+          classifier = Some(coordinates(classifier))
+        )
+    }
+
+
+  }
+
+
+}

dependency-resolver/maven-dependency-resolver-api/src/main/scala/com/wixpress/build/maven/MavenScope.scala
@@ -1,0 +1,19 @@
+package com.wix.build.maven
+
+abstract class MavenScope(val name: String)
+
+object MavenScope {
+
+  case object Runtime extends MavenScope("runtime")
+
+  case object Compile extends MavenScope("compile")
+
+  case object Test extends MavenScope("test")
+
+  case object Provided extends MavenScope("provided")
+
+  private val allScopes = List(Runtime, Compile, Test, Provided)
+
+  def of(scopeName: String): MavenScope = allScopes.find(_.name.equalsIgnoreCase(scopeName))
+    .getOrElse(MavenScope.Compile)
+}

dependency-resolver/maven-dependency-resolver-api/src/test/scala/com/wixpress/build/maven/DependencyTest.scala
@@ -1,0 +1,27 @@
+package com.wix.build.maven
+
+import org.specs2.mutable.SpecificationWithJUnit
+import org.eclipse.aether.graph.{Dependency => AetherDependency}
+
+class DependencyTest extends SpecificationWithJUnit {
+  val coordinates = new Coordinates("some.group","some-artifact","some-version")
+  "Dependency" should {
+    "convert from aether dependency" in {
+      val aetherDep = new AetherDependency(coordinates.asAetherArtifact,"")
+      Dependency.fromAetherDependency(aetherDep) mustEqual Dependency(coordinates,MavenScope.Compile)
+    }
+
+    "convert to aether dependency" in {
+      val dependency = Dependency(coordinates,MavenScope.Compile)
+
+      val aetherDependency = dependency.asAetherDependency
+
+      aetherDependency.getArtifact.getGroupId mustEqual coordinates.groupId
+      aetherDependency.getArtifact.getArtifactId mustEqual coordinates.artifactId
+      aetherDependency.getArtifact.getVersion mustEqual coordinates.version
+      aetherDependency.getArtifact.getClassifier mustEqual coordinates.classifier.getOrElse("")
+      aetherDependency.getArtifact.getExtension mustEqual coordinates.packaging.get
+      aetherDependency.getScope mustEqual dependency.scope.name
+    }
+  }
+}

dependency-resolver/maven-dependency-resolver-testkit/src/main/scala/com/wixpress/build/maven/FakeMavenRepository.scala
@@ -1,0 +1,54 @@
+package com.wix.build.maven
+
+import java.io.ByteArrayInputStream
+
+import org.apache.commons.codec.digest.DigestUtils
+import org.codehaus.mojo.mrm.api.maven.Artifact
+import org.codehaus.mojo.mrm.impl.maven.{ArtifactStoreFileSystem, MemoryArtifactStore}
+import org.codehaus.mojo.mrm.plugin.FileSystemServer
+
+class FakeMavenRepository(port: Int = 0) {
+
+  implicit class ExtendedArtifactDescriptor(artifact: ArtifactDescriptor) {
+    def asArtifact(ofType: String): Artifact = {
+      val parent = artifact.parentCoordinates
+      val groupId = artifact.groupId
+        .getOrElse(parent.map(_.groupId).getOrElse( throw new RuntimeException("missing groupId or parent.groupId")))
+      val version =  artifact.version
+        .getOrElse(parent.map(_.version).getOrElse( throw new RuntimeException("missing version or parent.version")))
+      new Artifact(groupId, artifact.artifactId, version, ofType)
+    }
+  }
+
+  private val inMemoryArtifactStore = new MemoryArtifactStore
+  private val mavenRepoManager = new FileSystemServer("foo", port,
+    new ArtifactStoreFileSystem(inMemoryArtifactStore), "")
+
+  def url: String = mavenRepoManager.getUrl
+
+  def start(): Unit = mavenRepoManager.ensureStarted()
+
+  def stop(): Unit = {
+    mavenRepoManager.finish()
+    mavenRepoManager.waitForFinished()
+  }
+
+  def addArtifacts(artifact: ArtifactDescriptor*): Unit = addArtifacts(artifact.toSet)
+
+  def addArtifacts(artifacts: Set[ArtifactDescriptor]): Unit = artifacts.foreach(addSingleArtifact)
+
+  def addSingleArtifact(artifact: ArtifactDescriptor): Unit = {
+    val xml = artifact.pomXml
+    val md5 = DigestUtils.md5Hex(xml)
+    val sha1 = DigestUtils.sha1Hex(xml)
+    inMemoryArtifactStore.set(artifact.asArtifact(ofType = "pom"), streamFrom(xml))
+    inMemoryArtifactStore.set(artifact.asArtifact(ofType = "pom.md5"), streamFrom(md5))
+    inMemoryArtifactStore.set(artifact.asArtifact(ofType = "pom.sha1"), streamFrom(sha1))
+  }
+
+
+  private def streamFrom(input: String) = {
+    new ByteArrayInputStream(input.getBytes("UTF-8"))
+  }
+
+}

dependency-resolver/maven-dependency-resolver-testkit/src/test/scala/com/wixpress/build/maven/ArtifactDescriptorTest.scala
@@ -1,0 +1,60 @@
+package com.wix.build.maven
+
+import org.specs2.mutable.SpecificationWithJUnit
+import org.specs2.matcher.XmlMatchers._
+
+import scala.xml._
+
+class ArtifactDescriptorTest extends SpecificationWithJUnit {
+  val baseCoordiantes = Coordinates("some.group", "some-artifact", "some-version")
+  val baseDescriptor = ArtifactDescriptor.anArtifact(baseCoordiantes)
+  val depCoordinates = Coordinates("some.group", "some-dep", "other-version")
+  val dependency = Dependency(depCoordinates, MavenScope.Compile)
+
+  "ArtifactDescriptor" should {
+    "reconstruct itself with new  dependency" in {
+      baseDescriptor.withDependency(dependency).dependencies must contain(dependency)
+    }
+
+    "reconstruct itself with new managed dependency" in {
+      baseDescriptor.withManagedDependency(dependency).managedDependencies must contain(dependency)
+    }
+
+    "build pom.xml" in {
+      val expected: Elem = <project xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd"
+                                    xmlns="http://maven.apache.org/POM/4.0.0"
+                                    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+                            <modelVersion>4.0.0</modelVersion>
+                            <groupId>{baseCoordiantes.groupId}</groupId>
+                            <artifactId>{baseCoordiantes.artifactId}</artifactId>
+                            <version>{baseCoordiantes.version}</version>
+                          </project>
+
+      val pom: Elem = XML.loadString(baseDescriptor.pomXml)
+
+      pom must beEqualToIgnoringSpace(expected)
+    }
+
+    "build pom.xml with dependency and exclusions" in {
+      val exclusion = Exclusion("excluded.group","excluded-artifact")
+      val dependencyWithExclusions = dependency.copy(exclusions=Set(exclusion))
+      val expected = <dependency>
+        <groupId>{dependencyWithExclusions.coordinates.groupId}</groupId>
+        <artifactId>{dependencyWithExclusions.coordinates.artifactId}</artifactId>
+        <version>{dependencyWithExclusions.coordinates.version}</version>
+        <scope>{dependencyWithExclusions.scope.name}</scope>
+        <exclusions>
+          <exclusion>
+            <artifactId>{exclusion.artifactId}</artifactId>
+            <groupId>{exclusion.groupId}</groupId>
+          </exclusion>
+        </exclusions>
+      </dependency>
+      val pom: Elem = XML.loadString(baseDescriptor.withDependency(dependencyWithExclusions).pomXml)
+      val allDeps = pom \ "dependencies" \ "dependency"
+
+      allDeps aka "dependencies in generated pom" must not be empty
+      allDeps.head must beEqualToIgnoringSpace(expected)
+    }
+  }
+}

dependency-resolver/maven-dependency-resolver/src/it/scala/com/wixpress/build/maven/AetherMavenDependencyResolverIT.scala
@@ -1,0 +1,19 @@
+package com.wix.build.maven
+
+import org.specs2.specification.AfterEach
+
+//noinspection TypeAnnotation
+class AetherMavenDependencyResolverIT extends MavenDependencyResolverContract with AfterEach {
+  sequential
+  val fakeMavenRepository = new FakeMavenRepository()
+
+  override def resolverBasedOn(artifacts: Set[ArtifactDescriptor]) = {
+    fakeMavenRepository.addArtifacts(artifacts)
+    fakeMavenRepository.start()
+    new AetherMavenDependencyResolver(List(fakeMavenRepository.url))
+  }
+
+  override protected def after = {
+    fakeMavenRepository.stop()
+  }
+}